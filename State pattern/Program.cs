0ecfc9b5daa63a14bc95e5249f3758b4a97285ea
laboratorio <laboratorio@UPOAULA10618>
2023-01-26 10:02:24 -0500

    Agregar archivos de proyecto.

---
+++ State pattern/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace State.Structural
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var context = new Context(new ConcreteStateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    public class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    public class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    public class Context
+    {
+        State state;
+
+        public Context(State state)
+        {
+            this.State = state;
+        }
+
+        public State State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                Console.WriteLine("State: " + state.GetType().Name);
+            }
+        }
+        public void Request()
+        {
+            state.Handle(this);
+        }
+    }
+}
